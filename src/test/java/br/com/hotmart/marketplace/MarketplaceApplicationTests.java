dab6b6ebb64e39c174380b9426ebf04c315143b1
Gabriela Mendonca de Carvalho <gabriela.carvalho@squadra.com.br>
2020-09-20 13:23:28 -0300

    [Environment setting] Application creation - Spring Initialize

---
+++ src/test/java/br/com/hotmart/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hotmart.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
